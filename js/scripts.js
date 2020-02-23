a7fb441ddc3a7394c740822208ef02e324896c3d
louisenje <louisenje@gmail.com>
2020-02-23 20:42:06 +0300

    created an alert that returns customer name and gives a feedback

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@
         $(this).children('.hide').slideToggle(1200);
        $(this).children('.show').toggle(1500);
     });
+     // giving feedback to the customer
+     $("button").click(function(omonge) {
+        var client = document.getElementById('nm').value;
+        alert(' Dear '  + client + ' we have received your message successful. ');
+        omonge.preventDefault();
+    });
+    //    reset my form
+      $("button").on('click', function(){
+        $('form').each(function(){
+          this.reset();
+        });
+    });
