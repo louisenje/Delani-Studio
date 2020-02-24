4ab37fda1e89bf9c1f56895950c6272dec08cce0
louisenje <louisenje@gmail.com>
2020-02-24 09:39:25 +0300

    code indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 // giving feedback to the customer
-$("button").click(function(omonge) {
+    $("button").click(function(omonge) {
     var client = document.getElementById('nm').value;
     alert(' Dear '  + client + ' we have received your message.' + 'Thank you for reaching out to us. ');
     omonge.preventDefault();
-});
+    });
 //    what we do toggle
     $('.main').click(function(){
-       $(this).children('.hide').slideToggle(1200);
-       $(this).children('.show').toggle(1500);
+    $(this).children('.hide').slideToggle(1200);
+    $(this).children('.show').toggle(1500);
     });
 
 // reset my form
     $("button").on('click', function(){
-        $('form').each(function(){
-          this.reset();
+    $('form').each(function(){
+    this.reset();
     });
     });
     $(".card").hover(function () {
-        $(this).children(".card-1").fadeToggle("fast", "linear");
+    $(this).children(".card-1").fadeToggle("fast", "linear");
     });
 
